b15dd87f3546bce598e99164a85903fb381638bd
Thiago Moreira <contatothiagomoreira@outlook.com>
2022-02-15 18:54:00 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+// Initialize modules
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const browsersync = require('browser-sync').create();
+
+// Sass Task
+function scssTask() {
+    return src('app/scss/style.scss', { sourcemaps: true })
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browsersync
+function browserSyncServe(cb) {
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+            },
+        },
+    });
+    cb();
+}
+function browserSyncReload(cb) {
+    browsersync.reload();
+    cb();
+}
+
+// Watch Task
+function watchTask() {
+    watch('*.html', browserSyncReload);
+    watch(['app/scss/**/*.scss'], series(scssTask, browserSyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, browserSyncServe, watchTask);
